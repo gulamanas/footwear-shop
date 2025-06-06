ec7159f9b2c8ea298758f06e858d6d0d31f88f0d
Gulam Anas <g.anas786@gmail.com>
2025-06-06 17:56:41 +0530

    home page changes

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,12 +1,30 @@
+import './HomePage.css';
 import { Link } from "react-router-dom"
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
-            <Link to="/stocks">Stocks</Link>
-            <Link to="/customers">Customer</Link>
-        </div>
+        <main className='main'>
+            <div className="header">
+                <h1>Agra Footwear</h1>
+                <div className="sub_header">
+                    <p>Address</p>
+                    <p>GST NO</p>
+                </div>
+            </div>
+            <div className="content">
+                <p>LISTS</p>
+                <div className="link_items">
+                    <Link to="/stocks">Stocks</Link>
+                    <Link to="/customers">Customers</Link>
+                </div>
+
+                <p>Add</p>
+                <div className="link_items">
+                    <Link to="/add-stock">Add Stock</Link>
+                    <Link to="/add-customer">Add Customer</Link>
+                </div>
+            </div>
+        </main>
     )
 }
 
